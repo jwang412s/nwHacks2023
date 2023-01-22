3405b2c26da8122ff0fd25ffef1fc555bd53894a
Roy <roy.rtao@gmail.com>
2023-01-21 18:22:04 -0800

    Created the basic search mechanics

---
+++ nwHacks2023.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+CLIENT_ID = '833b6467092e126472037b42fa9794f8'
+# url = 'https://api.myanimelist.net/v2/anime?q=Bleach&limit=1'
+url = 'https://api.myanimelist.net/v2/anime?q='
+limit = '&limit=5'
+animeNames = []
+
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+def nameappend(obj):
+    # create a list of anime from the response
+    for d in obj.json()['data']:
+        name = d['node']['title']
+        animeNames.append(name)
+
+# response = requests.get(url, headers={'X-MAL-CLIENT-ID': CLIENT_ID})
+
+
+recommendedAnimeOne = input('Search:\n')
+recommendedAnimeOneNumber = 0
+response1 = requests.get(url + recommendedAnimeOne + limit, headers={'X-MAL-CLIENT-ID': CLIENT_ID})
+nameappend(response1)
+while recommendedAnimeOneNumber > 5 or recommendedAnimeOneNumber < 1:
+    print("Please input a value between 1 and 5")
+    recommendedAnimeOneNumber = int(input('1) ' + animeNames[0] + '\n'
+                                '2) ' + animeNames[1] + '\n'
+                                '3) ' + animeNames[2] + '\n'
+                                '4) ' + animeNames[3] + '\n'
+                                '5) ' + animeNames[4] + '\n'))
+# recommendedAnimeTwo = input("Enter Anime Number Two:")
+# response2 = requests.get(url + recommendedAnimeTwo + limit, headers={'X-MAL-CLIENT-ID': CLIENT_ID}).status_code == 400
+# recommendedAnimeOne = input("1) " +  + "\n"
+#                             "2) " +  + "\n"
+#                             "3) " +  + "\n"
+#                             "4) " +  + "\n"
+#                             "5) " +  + "\n")
+
+# jprint(response.json());
